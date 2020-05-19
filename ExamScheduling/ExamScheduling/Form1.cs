9d99903c4c6287e00484c150b5107bbc00075c60
TwT-Teaker99 <49748759+TwT-Teaker99@users.noreply.github.com>
2020-05-19 15:02:49 +0700

    Upload c#

---
+++ ExamScheduling/ExamScheduling/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamScheduling
+{
+    public partial class Form1 : Form
+    {
+        ToMau tb = new ToMau();
+        DataTable table = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void DBAccess()
+        {
+            string con = @"Data Source=ADMIN\SQLEXPRESS;Initial Catalog=LICHTHI;Integrated Security=True";
+            SqlConnection Con = new SqlConnection(con);
+            SqlDataAdapter adapter = new SqlDataAdapter("exec P1", con);
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+
+
+        }
+        private void ToMau()
+        {
+            int n = table.Rows.Count;
+            List<HocPhan> HP = new List<HocPhan>();
+            HocPhan hp0 = new HocPhan();
+            HP.Add(hp0);
+            for (int i = 0; i < n; i++)
+            {
+                HocPhan hp = new HocPhan(Convert.ToInt32(table.Rows[i]["MaMon"]));
+                string s = table.Rows[i]["MaSV"].ToString().TrimEnd(',');
+                string[] arrS = s.Split(',');
+                int j = 0;
+                foreach (string arr in arrS)
+                {
+                    hp.addSV(Convert.ToInt32(arr));
+                    j++;
+                }
+                HP.Add(hp);
+            }
+            //show danh sach sv cua lop hp
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for(int j=0;j<HP[i+1].Sv.Count;j++)
+            //    {
+            //        label1.Text = label1.Text + HP[i+1].Sv[j]+"  ";
+            //    }
+            //    label1.Text += "\n";
+            //}
+
+
+            int[,] H = new int[n + 1, n + 1];
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    H[i, j] = 0;
+                }
+            }
+
+            tb.convertDS1(n, HP, H);
+            int[] Bac = new int[10];
+            tb.BacDinh(H, n, Bac);
+            int[] Dinh = new int[10];
+            tb.MangDinh(n, Dinh);
+            tb.SortDinh(Dinh, Bac, n);
+            tb.Tomau1(H, Dinh, n, HP);
+
+            for (int i = 1; i <= n; i++)
+            {
+                label1.Text = label1.Text + HP[i].show();
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            DBAccess();
+
+            ToMau();
+            
+        }
+    }
+}
---
+++ ExamScheduling/ExamScheduling/HocPhan.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamScheduling
+{
+    class HocPhan
+    {
+        private int maHP, color=0, tgThi=1, maP=0;
+        private List<int> sv;
+        public HocPhan(int ma)
+        {
+            sv = new List<int>();
+            this.maHP = ma;
+        }
+        public HocPhan()
+        { }
+        public int MaHP { get => this.maHP; set => this.maHP = value; }
+        public int Color { get => color; set => color = value; }
+        public List<int> Sv { get => sv; set => sv = value; }
+        public void addSV(int a)
+        {
+            this.sv.Add(a);
+        }
+        public string show()
+        {
+            return "Mã HP: " + maHP + " / Màu: " + Color + "\n";
+        }
+    }
+}
